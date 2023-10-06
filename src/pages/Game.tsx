471d7f82c26c31e0e15ec4dfac3a5ab60b6080bd
Hellscythe25 <46532421+Hellscythe25@users.noreply.github.com>
2023-10-05 21:16:36 -0500

    React > Unity Comunication

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,18 +1,25 @@
 import {Unity, useUnityContext} from "react-unity-webgl";
 
 function Game() {
-    const { unityProvider } = useUnityContext({
+    const { unityProvider, sendMessage } = useUnityContext({
         loaderUrl: "/UnityReact.loader.js",
         dataUrl: "/UnityReact.data.unityweb",
         frameworkUrl: "/UnityReact.framework.js.unityweb",
         codeUrl: "/UnityReact.wasm.unityweb",
     });
+
+    function handleClickSpawnEnemies() {
+        sendMessage("GameObject", "SpawnEnemies");
+    }
+
+
     return (
         <>
             <div className="centered-container">
                 <div className="centered-content">
                     <h1 className="centered-title">React + Unity / Tecsup</h1>
                     <Unity unityProvider={unityProvider} className="centered-unity" />
+                    <button onClick={handleClickSpawnEnemies}>Spawn Enemies</button>
                 </div>
             </div>
 
