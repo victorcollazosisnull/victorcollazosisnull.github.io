6415d6b599d641fd96bbae18c2ac3563223ef065
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2025-04-24 23:09:59 -0500

    TestWebGame

--- src/common/MainNav.tsx
+++ src/common/MainNav.tsx
@@ -8,14 +8,14 @@ function MainNav() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/">Menu</Link>
+                            <a className="nav-link" href="/">Menu</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/game">Prototype</Link>
+                            <a className="nav-link" href="/game">Prototype</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/game2">Prototype2</Link>
-                            </li>
+                            <a className="nav-link" href="/game2">Prototype2</a>
+                        </li>
                     </ul>
                 </div>
             </div>
--- src/pages/Game2.tsx
+++ src/pages/Game2.tsx
@@ -1,43 +1,53 @@
 import { Unity, useUnityContext } from "react-unity-webgl";
-
+import { useEffect } from "react";
 
 function Game2() {
-    const { unityProvider, sendMessage } = useUnityContext({
-        loaderUrl: "/Game2/cloud.loader.js",
-        dataUrl: "/Game2/cloud.data",
-        frameworkUrl: "/Game2/cloud.framework.js",
-        codeUrl: "/Game2/cloud.wasm",
+    const { unityProvider, sendMessage, unload } = useUnityContext({
+        loaderUrl: "/Game2/CardGame.loader.js",
+        dataUrl: "/Game2/CardGame.data",
+        frameworkUrl: "/Game2/CardGame.framework.js",
+        codeUrl: "/Game2/CardGame.wasm",
     });
 
-    let name: string = "VJCR"
-
+    let name: string = "VJCR";
 
     function handleSceneRestart() {
         sendMessage("SceneManager", "ReloadScene");
     }
 
-    function sendName()
-    {
+    function sendName() {
         sendMessage("SceneManager", "ChangeText", name);
     }
 
+    useEffect(() => {
+        return () => {
+            console.log("Desmontando Game2, descargando Unity...");
+    
+            unload().then(() => {
+                const canvas = document.querySelector("canvas");
+                if (canvas) {
+                    canvas.remove(); 
+                    console.log("Canvas eliminado correctamente");
+                }
+            });
+        };
+    }, []);
+
     return (
-        <>
-            <div className="centered-container">
-                <div className="centered-content">
-                    <h1 className="centered-title">Game 2</h1>
-                    <Unity unityProvider={unityProvider} className="centered-unity" />
+        <div className="centered-container">
+            <div className="centered-content">
+                <h1 className="centered-title">Game 2</h1>
+                <Unity unityProvider={unityProvider} className="centered-unity" />
                 <div className="centered-content">
                     <button onClick={handleSceneRestart}>Restart Scene</button>
                     <button onClick={sendName}>Send Name</button>
                 </div>
-                </div>
-                <div>
-                    <h1>Game 2</h1>
-                    <p>This is the second game.</p>
-                </div>
             </div>
-        </>
+            <div>
+                <h1>Game 2</h1>
+                <p>This is the second game.</p>
+            </div>
+        </div>
     );
 }
 
