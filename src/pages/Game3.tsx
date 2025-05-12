fcab084d3195deb7076cb4c2e03fda28b0208733
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2025-05-11 20:59:33 -0500

    Game3

--- src/pages/Game3.tsx
+++ src/pages/Game3.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game3() {
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "/Game3/Game3.loader.js",
+        dataUrl: "/Game3/Game3.data",
+        frameworkUrl: "/Game3/Game3.framework.js",
+        codeUrl: "/Game3/Game3.wasm",
+    });
+
     return (
         <div className="centered-container">
             <div className="centered-content">
                 <h1 className="centered-title">Game 3</h1>
-                <p>This is the third game. Content will be added soon.</p>
+
+                {/* AQUÍ debes poner el nuevo Unity con tamaño fijo */}
+                <Unity
+                    unityProvider={unityProvider}
+                    style={{ width: "394px", height: "434px" }}
+                />
+
             </div>
         </div>
     );
 }
-
 export default Game3;
