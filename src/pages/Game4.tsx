6acc5f571a870f93ba76ec58ac7bb2d1c2cc42b7
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2025-05-13 16:56:37 -0500

    Game4

--- src/pages/Game4.tsx
+++ src/pages/Game4.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game4() {
-    return (
-        <div className="centered-container">
-            <div className="centered-content">
-                <h1 className="centered-title">Game 4</h1>
-                <p>This is the fourth game. Content will be added soon.</p>
-            </div>
-        </div>
-    );
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/Game4/Game4.loader.js",
+    dataUrl: "/Game4/Game4.data",
+    frameworkUrl: "/Game4/Game4.framework.js",
+    codeUrl: "/Game4/Game4.wasm",
+  });
+
+  return (
+    <div className="centered-container">
+      <div className="centered-content">
+        <h1 className="centered-title">Game 4</h1>
+
+        {/* AQUÍ debes poner el nuevo Unity con tamaño fijo */}
+        <Unity
+          unityProvider={unityProvider}
+          style={{ width: "394px", height: "434px" }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Game4;
