32f83e7b34cd5e117f76f98f99c72c1861c9aa9f
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2025-05-17 20:42:07 -0500

    RestructurinGames

--- src/pages/Game4.tsx
+++ src/pages/Game4.tsx
@@ -1,26 +1,24 @@
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game4() {
-  const { unityProvider } = useUnityContext({
-    loaderUrl: "/Game4/Game4.loader.js",
-    dataUrl: "/Game4/Game4.data",
-    frameworkUrl: "/Game4/Game4.framework.js",
-    codeUrl: "/Game4/Game4.wasm",
-  });
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "/Game4/Game4.loader.js",
+        dataUrl: "/Game4/Game4.data",
+        frameworkUrl: "/Game4/Game4.framework.js",
+        codeUrl: "/Game4/Game4.wasm",
+    });
 
-  return (
-    <div className="centered-container">
-      <div className="centered-content">
-        <h1 className="centered-title">Game 4</h1>
+    return (
+        <div className="centered-container">
+            <div className="centered-content">
+                <h1 className="centered-title">Game 4</h1>
 
-        {/* AQUÍ debes poner el nuevo Unity con tamaño fijo */}
-        <Unity
-          unityProvider={unityProvider}
-          style={{ width: "1280px", height: "720px" }}
-        />
-      </div>
-    </div>
-  );
+                <div className="unity-wrapper">
+                    <Unity unityProvider={unityProvider} className="responsive-unity" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Game4;
