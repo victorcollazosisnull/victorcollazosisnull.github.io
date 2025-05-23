f41ee380d6d242bbba598400ffe60f9860c01f4f
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2025-05-22 23:31:07 -0500

    eventReact

--- src/pages/Game3.tsx
+++ src/pages/Game3.tsx
@@ -1,26 +1,39 @@
+import React, { useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game3() {
-    const { unityProvider } = useUnityContext({
-        loaderUrl: "/Game3/Game3.loader.js",
-        dataUrl: "/Game3/Game3.data",
-        frameworkUrl: "/Game3/Game3.framework.js",
-        codeUrl: "/Game3/Game3.wasm",
-    });
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/Game3/Game3.loader.js",
+    dataUrl: "/Game3/Game3.data",
+    frameworkUrl: "/Game3/Game3.framework.js",
+    codeUrl: "/Game3/Game3.wasm",
+  });
 
-    return (
-        <div className="centered-container">
-            <div className="centered-content">
-                <h1 className="centered-title">Game 3</h1>
+  const [mensaje, setMensaje] = useState("Esperando mensaje...");
 
-                {/* AQUÍ debes poner el nuevo Unity con tamaño fijo */}
-                <Unity
-                    unityProvider={unityProvider}
-                    style={{ width: "394px", height: "434px" }}
-                />
+  useEffect(() => {
+    window.SendDataToReact = function (msg) {
+      console.log("Mensaje recibido de Unity:", msg);
+      setMensaje(msg);
+    };
 
-            </div>
+    return () => {
+      delete window.SendDataToReact;
+    };
+  }, []);
+
+  return (
+    <div className="centered-container">
+      <div className="centered-content">
+        <h1 className="centered-title">Game 3</h1>
+        <Unity unityProvider={unityProvider} className="responsive-unity" />
+        <div>
+          <h2>Mensaje desde Unity:</h2>
+          <p>{mensaje}</p>  {}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
+
 export default Game3;
